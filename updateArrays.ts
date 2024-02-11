9bffb3373f7185baf63dba10142a0bab8829011c
xGiova <77775883+xGiova@users.noreply.github.com>
2024-02-11 22:28:37 +0100

    Update and rename updateArrays to updateArrays.ts

--- updateArrays.ts
+++ updateArrays.ts
@@ -20,6 +20,8 @@
     setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
 
     // Update Array of Objects with Immer
+    // draft is a proxy object that records the changes that we are going to apply to the array desired.
+    // draft is a copy to draft array, so we are free to mutate or modify.
     setBugs(produce(draft => {
       const bug = draft.find(bug => bug.id === 1);
       if (bug) bug.fixed = true; // if bug is true, we update the boolean expression to true.
